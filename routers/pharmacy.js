67581aeb8dc20ec327e857962fcce2d09605eaac
Baqaisooo <aboali_6438@hotmail.com>
2022-03-16 03:56:39 +0300

    Create Pharmacies page

--- routers/pharmacy.js
+++ routers/pharmacy.js
@@ -1,4 +1,14 @@
 
+
+// firebase-admin connection, to access firebase from server-side
+var admin = require("firebase-admin");
+var serviceAccount = require("../pharmacy-fb5d1-firebase-adminsdk-b5f1e-0c8fcf81aa.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://pharmacy-fb5d1-default-rtdb.firebaseio.com"
+});
+
+
 const express = require('express');
 
 const router = express.Router();
@@ -7,18 +17,15 @@ const router = express.Router();
 router.get('/', (req, res) => {
     // get all pharmacies
     
+    // As an admin, the app has access to read and write all data, regardless of Security Rules
+    var db = admin.database();
+    var ref = db.ref("Pharmacies");
+    ref.once("value", function(snapshot) {
+        const data = snapshot.val();
+        res.render("./pharmacy/homepage", {pharmacies:data})
+    });
 
-    res.render("./pharmacy/homepage")
 });
 
-// Middleware 
-function loggedIn(req, res, next) {
-    // check if logged in then show the pharmacies otherwise redirected to login page
-    if (true) {
-        next()
-    } else {
-        
-    }
-}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 
 
-
-
-
-
-
-
-
 const express = require("express")
 const app = express()
 
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("./pharmacy-fb5d1-firebase-adminsdk-b5f1e-0c8fcf81aa.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://pharmacy-fb5d1-default-rtdb.firebaseio.com"
-// });
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : true}));
@@ -49,6 +34,3 @@ app.get('/', (req, res) => {
 
 app.listen(3000);
 
-
-
-// module.exports = admin
